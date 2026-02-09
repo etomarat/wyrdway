3dd13f8851ded3846005901e303aec043da9fac5
Azizov Marat <etomarat@gmail.com>
2026-02-09 15:41:15 +0300

    refactor(drive): extract debug overlay lines into drive_debug_lines

--- tic80/python/scenes/drive_scene.py
+++ tic80/python/scenes/drive_scene.py
@@ -14,6 +14,7 @@ if TYPE_CHECKING:
     from ..systems.drive.drive_obstacle_hits import apply_obstacle_hits
     from ..systems.drive.drive_objects import DriveObjects, DriveZone
     from ..systems.drive.drive_telemetry import DriveTelemetry
+    from ..systems.drive.drive_debug_lines import drive_debug_lines
     from ..systems.drive.drive_zone_effects import apply_zone_effects
     from ..systems.drive.drive_zones import zone_at_hitboxes
     from ..systems.drive.road_model import RoadModel
@@ -191,50 +192,8 @@ class DriveScene:
         else:
             print("ARROWS + X", 2, 128, Color.WHITE)
         self._state.set_debug_lines(
-            self._drive_debug_lines(road, logic, run, objects))
+            drive_debug_lines(road, logic, run, objects, TUNING))
         return
-
-    def _drive_debug_lines(
-        self,
-        road: RoadModel,
-        logic: DriveLogic,
-        run: RunState,
-        objects: DriveObjects
-    ) -> list[str]:
-        """Строки для DebugOverlay, чтобы HUD не накладывался на оверлей."""
-        def f2(v: float) -> str:
-            return f"{v:.2f}"
-
-        vmax_road = logic.estimated_vmax_road()
-        vmax_off = logic.estimated_vmax_offroad()
-
-        lines = [
-            "drive seed=" + str(run.seed) + " obs=" +
-            str(objects.obstacles_count())
-            + " zones=" + str(objects.zones_count()),
-            "drive s=" + str(int(logic.road_s)) + "/" +
-            str(int(road.segment_total_length)),
-            "drive d=" + f2(logic.road_d),
-            "drive v=" + f2(logic.v_forward) + " side=" + f2(logic.v_side),
-            "drive spd=" + f2(logic.speed) + " vmax=" +
-            f2(vmax_road) + "/" + f2(vmax_off),
-            "drive surf=" + ("OFF" if logic.offroad else "ROAD")
-            + " sf=" + f2(logic.dbg_speed_factor)
-            + " ss=" + f2(logic.dbg_steer_scale)
-            + " hb=" + f2(logic.dbg_handbrake_decel),
-            "drive grip=" + f2(logic.dbg_effective_grip) +
-            " damp=" + f2(logic.dbg_side_damp)
-            + " rec=" + f2(logic.dbg_side_recovery)
-            + " fuel/s=" + f2(logic.dbg_fuel_per_sec),
-            "drive boost fwd=" + f2(logic.dbg_zone_boost_forward)
-            + " ctr=" + f2(logic.dbg_zone_boost_center)
-            + " as=" + f2(logic.dbg_zone_antislip),
-            "drive fuel=" + f2(run.car_fuel),
-            "drive hp=" + f2(run.car_hp)
-        ]
-        if logic.offroad:
-            lines.append("drive OFFROAD")
-        return lines
 
     def exit(self) -> None:
         pass
